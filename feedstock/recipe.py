d3c2a1950c54e0e035c1f72372aefeb2d756b119
Nathan Zimmerman <npzimmerman@gmail.com>
2024-05-08 13:17:21 -0500

    I don't know, maybe the trailing slash

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -96,7 +96,7 @@ class RechunkPerFile(beam.PTransform):
 
     def write_intermediate_chunked(self, ds: xr.Dataset) -> xr.Dataset:
         rechunked_ds = ds.chunk(self.target_chunks)
-        zarrstore = f's3://veda-pforge-emr-intermediate-store/{uuid.uuid1()}/'
+        zarrstore = f's3://veda-pforge-emr-intermediate-store/{uuid.uuid1()}'
         fs_intermediate = s3fs.S3FileSystem(**target_fsspec_kwargs)
         s3mapping = s3fs.S3Map(zarrstore, fs_intermediate)
         rechunked_ds.to_zarr(s3mapping)
